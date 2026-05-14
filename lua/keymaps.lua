efb741e48bb76211089b7750a1b76df5866a2039
liuyameng <liuyameng@dreame.tech>
2026-05-14 15:46:24 +0800

    add esc keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,11 +5,16 @@ local opts = {
 
 vim.g.mapleader = " "
 
+-- map esc
+vim.keymap.set('i', "<Space><Space>", "<ESC>", { desc = "Exit Insert Mode" })
+
+-- resize window
 vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', opts)
 vim.keymap.set('n', '<C-Down', ':resize +2<CR>', opts)
 vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
 vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
 
+-- indent
 vim.keymap.set('v', '<', '<gv', opts)
 vim.keymap.set('v', '>', '>gv', opts)
 
